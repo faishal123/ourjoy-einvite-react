148c7459e92b38713f3738edc3c36c9112b02145
FaishalJitera <faishal.rahman@jitera.com>
2024-08-21 05:25:39 +0700

    fix: change favicon and add meta tags for link preview

--- src/Components/Shared/Flower/Flower.tsx
+++ src/Components/Shared/Flower/Flower.tsx
@@ -26,10 +26,7 @@ const Flower = ({
       style={{ top, left, rotate, width, maxWidth, zIndex, height, maxHeight }}
       className={styles.flower}
     >
-      <img
-        alt=""
-        src="https://s3-alpha-sig.figma.com/img/2ba5/51f2/201b9be3d6d32e2d2293e363f4bbc45b?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=V0EJwEVeVdKn0VUqxabjH17aaTUQd1vSwKf7E1~c88UjwZQZW--qpj788nBhLOre7aCLEeHONj7RqKNnwrKAb60RmKn58lNaDRBIhl3Yr4XHfhT7IUYJvX4nfsN6xCxbEmYhI5JZqL0dBMb~SSR3g0W82vU~BkTGwvVHbN0dxFC~h-2ph~srCz2cmYGlQFeozGoGQLUq5Vr-da5Jdx-pyLZ8TFA7Q9rO7dinPXAYVOoEsw0Wao~ueeIxA2jrY-fnedBdWegIsZGmpXKhu7gDvfrRo~8~topcqdnbzxaU7~MT1rxuoh9Q0dNp-gRCOWuFP1MUzEUXG55CWeyODlE7OA__"
-      />
+      <img alt="" src="https://utfs.io/f/da7a77a0-46a0-4d61-9e6c-3563586d0a46-l13fdx.png" />
     </div>
   );
 };
